393fbc941442937bf5b1dde850c9386af2ecfd0b
JeroenvanderRhee <32259558+JeroenvanderRhee@users.noreply.github.com>
2018-10-01 16:06:18 +0200

    1.0.11 Fix css bug

--- techcommunity/resources/views/pages/news.blade.php
+++ techcommunity/resources/views/pages/news.blade.php
@@ -1,21 +1,31 @@
 <h1>News</h1>
 
 <style>
-article{
+#post{
 	margin-top:10px;
 	margin-bottom:10px;
-	border:1px solid black;
+	-webkit-box-shadow: 2px 2px 20px -4px rgba(0,0,0,0.67);
+	-moz-box-shadow: 2px 2px 20px -4px rgba(0,0,0,0.67);
+	box-shadow: 2px 2px 20px -4px rgba(0,0,0,0.67);
+
+	height:150px;
 }
 
 img{
-	max-width:250px;
+	max-width:230px;
 	float:left;
+	max-height:120px;
+	padding:10px;
+}
 
+.text-section{
+	width:calc(100% - 250px);
+	float:right;
 }
 h2{
 	font-size: 20pt;
     font-family: "Ar Cena";
-    padding: 10px;
+    padding: 10px !important;
     margin: 0px;
     color:#0D8DFF;	
 }
@@ -23,9 +33,7 @@ h2{
 p{
 	padding:10px;
 	font-weight:500;
-	margin:0;
-	float:right;
-	width:calc(100% - 250px);
+	margin:0px;
 }
 
 a{
@@ -33,6 +41,7 @@ a{
 	font-weight:500;
 	text-decoration:none;
 	padding:10px;
+	padding-top:40px;
 }
 
 a:hover{
@@ -42,16 +51,17 @@ a:hover{
 
 </style>
 
-<article>
+<div id="post">
 	<img src="http://www.wassenaarders.nl/images/2016/08/11/dory_large.jpg"/>
-	<h2>
-	{{$title}}
-	</h2>
-	<p>
-		{{$description}}
-	</p>
-
-	<a href="/posts/{{$url}}">Read more...</a>
 	
-</article>
+	<div class="text-section">
+		<h2>
+			{{$title}}
+		</h2>
+		<p>
+			{{$description}}
+		</p>
+		<a href="/posts/{{$url}}">Read more...</a>
+	</div>
+</div>
 		
--- techcommunity/routes/web.php
+++ techcommunity/routes/web.php
@@ -16,12 +16,15 @@ Route::get('/', function () {
 });
 
 Route::get('/news', function () {
+	$description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula augue id commodo auctor. Mauris in justo sollicitudin, suscipit augue non, cursus lacus. ";
+    $description = substr_replace ($description , "..." , 200);
+
 	$url = "1/";
 	$url .= str_replace(" ","-","Titel van dit project");
 	$data = array(
 		'id' => "1",
-		'title' => "Titel",
-		'description' => "Beschrijving",
+		'title' => "Titel van het project",
+		'description' => $description,
 		'url' => $url,);
     return view('pages.news') -> with($data);
 });
