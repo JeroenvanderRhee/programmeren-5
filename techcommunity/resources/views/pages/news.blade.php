d06e59db552ba9d22aa3f5beb8b0c5071f56813f
JeroenvanderRhee <32259558+JeroenvanderRhee@users.noreply.github.com>
2018-10-01 15:35:04 +0200

    1.0.10 template style toevoegen

--- techcommunity/resources/views/pages/news.blade.php
+++ techcommunity/resources/views/pages/news.blade.php
@@ -1,14 +1,57 @@
 <h1>News</h1>
 
-	<article>
-		<h2>
-		{{$title}}
-		</h2>
-		<p>
-			{{$description}}
-		</p>
-		<a href="/posts/{{$url}}">Read more...</a>
-	</article>
+<style>
+article{
+	margin-top:10px;
+	margin-bottom:10px;
+	border:1px solid black;
+}
+
+img{
+	max-width:250px;
+	float:left;
+
+}
+h2{
+	font-size: 20pt;
+    font-family: "Ar Cena";
+    padding: 10px;
+    margin: 0px;
+    color:#0D8DFF;	
+}
+
+p{
+	padding:10px;
+	font-weight:500;
+	margin:0;
+	float:right;
+	width:calc(100% - 250px);
+}
+
+a{
+	color:#0D8DFF;	
+	font-weight:500;
+	text-decoration:none;
+	padding:10px;
+}
+
+a:hover{
+	text-decoration: underline;
+}
+
+
+</style>
+
+<article>
+	<img src="http://www.wassenaarders.nl/images/2016/08/11/dory_large.jpg"/>
+	<h2>
+	{{$title}}
+	</h2>
+	<p>
+		{{$description}}
+	</p>
+
+	<a href="/posts/{{$url}}">Read more...</a>
+	
+</article>
 		
-		<p>
-		</p>
