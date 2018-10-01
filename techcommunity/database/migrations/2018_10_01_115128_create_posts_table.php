eb0d6dd9d122077e25e74b5cba40c5c23e20b39c
JeroenvanderRhee <32259558+JeroenvanderRhee@users.noreply.github.com>
2018-10-01 14:09:32 +0200

    1.0.8 Fix DB

---
+++ techcommunity/database/migrations/2018_10_01_115128_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->longText('description_post');
+            $table->longText('long_text');
+            $table->string('uploaded_file');
+            $table->dateTime('created_at_date');
+            $table->dateTime('created_by_user');
+            $table->ipAddress('ip_created_at');   
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
